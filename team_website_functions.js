28facd05d88180aa11954a2b5e9f3e2057567e65
Maxwell L Rahe <mr03989@srcs.loc>
2017-11-27 11:37:45 -0800

    yes

--- team_website_functions.js
+++ team_website_functions.js
@@ -8,10 +8,10 @@ function dateMessage(){
     var weekday = n.getDay();
     document.write("Today is ", dayOfWeek[weekday], ", "); //writes the Day of the week
     console.log(weekday);// 1
-
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var s = new Date();
-    var month = s.getMonth(); //gets the month number
-    document.write(s, " "); //displays month
+    var month = s.getMonth();//gets the month function
+    document.write(months[month], " "); //displays month
     var r = new Date();
     var dayNumber = r.getDate(); //gets day number
     document.write(dayNumber, "th"); //displays day
