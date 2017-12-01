201cec7de9620296a83011712ce920e1b3dbcdcc
Maxwell L Rahe <mr03989@srcs.loc>
2017-12-01 11:15:57 -0800

    added th, nd, st code segment

--- team_website_functions.js
+++ team_website_functions.js
@@ -3,6 +3,22 @@ function randomGreeting() {
 }
 
 function dateMessage() {
+
+        var date = new Date();
+    var day = date.getDate();
+    var ending;
+
+
+    if (day == 1 || day == 21 || day == 31) {
+        ending = "st";
+    } else if (day == 2 || day == 22) {
+        ending = "nd";
+    } else if (day == 3 || day == 23) {
+        ending = "rd";
+    } else {
+        ending = "th";
+    }
+
     var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // gets day of the week from here
     var n = new Date();
     var weekday = n.getDay();
@@ -14,7 +30,7 @@ function dateMessage() {
     document.write(months[month], " "); //displays month
     var r = new Date();
     var dayNumber = r.getDate(); //gets day number
-    document.write(dayNumber, "th"); //displays day
+    document.write(dayNumber, ending); //displays day
 }
 
 function BirthdayCountDown() {
