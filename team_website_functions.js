b3d22e12cb0cad4300aaf04f0e9f8340cd68ff07
Maxwell L Rahe <mr03989@srcs.loc>
2017-12-01 11:29:56 -0800

    cleaning up

--- team_website_functions.js
+++ team_website_functions.js
@@ -3,12 +3,9 @@ function randomGreeting() {
 }
 
 function dateMessage() {
-
     var dayThing = new Date();
     var day = dayThing.getDate();
     var ending;
-
-
     if (day == 1 || day == 21 || day == 31) {
         ending = "st";
     } else if (day == 2 || day == 22) {
@@ -18,12 +15,10 @@ function dateMessage() {
     } else {
         ending = "th";
     }
-
     var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // gets day of the week from here
     var n = new Date();
     var weekday = n.getDay();
     document.write("Today is ", dayOfWeek[weekday], ", "); //writes the Day of the week
-
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var s = new Date();
     var month = s.getMonth(); //gets the month function
@@ -33,6 +28,6 @@ function dateMessage() {
     document.write(dayNumber, ending); //displays day
 }
 
-function BirthdayCountDown() {
+function birthdayCountDown() {
 
 }
