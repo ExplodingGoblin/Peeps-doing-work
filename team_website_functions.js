22c9db902ef1ec78ca6c24a5903053f3aae5ed96
Maxwell L Rahe <mr03989@srcs.loc>
2017-12-05 11:02:17 -0800

    added in bday function

--- team_website_functions.js
+++ team_website_functions.js
@@ -28,6 +28,27 @@ function dateMessage() {
     document.write(dayNumber, ending); //displays day
 }
 
-function birthdayCountDown() {
+function birthdayCountDown(bdate) {
+
+    var today = new Date();
+    var bday = new Date(bdate);
+    var currenttime = today.getTime();
+    var btime = bday.getTime();
+
+    if ((btime - currenttime) < 0) {
+
+
+        bday.setFullYear(today.getFullYear());
+
+        if ((bday.getTime() - today.getTime()) < 0) {
+            bday.setFullYear(bday.getFullYear() + 1);
+        }
+
+    }
+
+    var daysuntil = Math.floor((bday.getTime() / 86400000) - (today.getTime() / 86400000));
+
+
+    document.write(daysuntil);
 
 }
